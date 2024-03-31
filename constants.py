d8d2276aa5b883747caf08a4a4aeb858af199b8b
Cara <carachip4@gmail.com>
2024-03-31 16:26:19 -0400

    update sheetId

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
 WORKSHEET_TITLE = "2024 Transactions"
-GOOGLE_SHEET_ID = "1JvfrpdSdhf7tHQiy3rv87orvBG7Su822feUHwbzoK4Y"
-DOWNLOADS_PATH = "~/Downloads/"
+GOOGLE_SHEET_ID = "1wnF2WVgfnDC9-oLQqXwkOjfWs66Yq3A5TNDafiD8c7M"
 GOOGLE_API_CREDENTIALS_FILE = "import-finances-59ac45b427a9.json"
