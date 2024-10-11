aed52cc7f3985c4f30b230df4059f1400dbcc451
Cara <carachip4@gmail.com>
2024-10-11 18:48:43 -0400

    generic constants on main

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 TRANSACTION_SHEET_TITLE = "2024 Transactions"
 CATEGORY_SHEET_TITLE = "Categories"
 GOOGLE_SHEET_ID = "your sheet id here"
-GOOGLE_API_CREDENTIALS_FILE = "import-finances-d4b7b2a81f11.json"
+GOOGLE_API_CREDENTIALS_FILE = "your import-finances.json file here"
