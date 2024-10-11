bda693eefb0afadba6a8eacd3a3743959a86c126
Cara <carachip4@gmail.com>
2024-10-11 18:44:47 -0400

    update constants

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 TRANSACTION_SHEET_TITLE = "2024 Transactions"
 CATEGORY_SHEET_TITLE = "Categories"
 GOOGLE_SHEET_ID = "your sheet id here"
-GOOGLE_API_CREDENTIALS_FILE = "import-finances-59ac45b427a9.json"
+GOOGLE_API_CREDENTIALS_FILE = "import-finances-d4b7b2a81f11.json"
